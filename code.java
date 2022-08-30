2909c62bf08423f4371ce14a9aa4324d05df9254
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-08-30 22:48:15 +0900

    내적[0830]

--- code.java
+++ code.java
@@ -1,13 +1,9 @@
 class code {
-    public double solution(int[] arr) {
-        double answer = 0;
-        double a=0;
-
-        for(int i=0; i<arr.length; i++){
-            a+=arr[i];
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i=0; i<a.length; i++){
+            answer+=a[i]*b[i];
         }
-        answer=a/arr.length;
-
         return answer;
     }
 }
