97a7c44f1389a83010febb3c72450db7fb2d136d
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-09-06 00:15:08 +0900

    음양 더하기[0906]

--- code.java
+++ code.java
@@ -1,19 +1,14 @@
 class code {
-    public int solution(int[][] sizes) {
-        int max_row=0; //가로 최대 길이
-        int max_col=0; //세로 최대 길이
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
 
-        for(int i=0; i<sizes.length; i++){
-            if(sizes[i][0]<sizes[i][1]){
-                int a=sizes[i][0];
-                sizes[i][0]=sizes[i][1];
-                sizes[i][1]=a;
+        for (int i = 0; i < signs.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
             }
-            if(max_row<sizes[i][0]) max_row = sizes[i][0];
-            if(max_col<sizes[i][1]) max_col = sizes[i][1];
         }
-
-        return max_row*max_col;
+        return answer;
     }
 }
-
